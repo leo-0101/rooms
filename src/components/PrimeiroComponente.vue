67a2fd72edd2b979b6b307924bd8ccf1e682fe54
Leo S. <contato.prof.leonardo@gmail.com>
2024-10-24 16:56:22 -0400

    Múltiplos eventos estáticos

--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -2,13 +2,20 @@
   <div>
     <h1>Olá Vue!</h1>
     <p>Meu nome é {{ name }} e trabalho como {{ job }}</p>
+    <MultEventos/>
   </div>
   
 </template>
 
 <script>
+  import MultEventos from './form/MultEventos.vue'
+
   export default {
     name: 'PrimeiroComponente',
+    components: {
+      MultEventos
+    },
+
     data() {
       return {
         name: 'Leonardo',
---
+++ src/components/form/MultEventos.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="first(), sec()">Ativar múltiplos eventos</button>
+    <p>{{ multiplosEventos }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MultEventos',
+    data() {
+      return {
+        multiplosEventos: ''
+      };
+    },
+
+    methods: {
+      first() {
+        this.multiplosEventos = 'Primeiro evento ativado!';
+      },
+      sec() {
+        setTimeout(() => this.multiplosEventos = 'Segundo evento!', 500)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  div {
+    margin: 100px;
+  }
+</style>
